11798f633992e52eaa05e75e3437cf66556d5a22
cristhofer-abuhadba <crisabuhadba@gmail.com>
2025-09-27 19:37:32 -0300

    Apartado de reporte hecho

--- php/env.php
+++ php/env.php
@@ -1,11 +1,11 @@
 <?php
 
-$SERVER = "DESKTOP-I20CH35"; // Escapa la barra invertida en el nombre del servidor
+$SERVER = "ACKERLY\SQLEXPRESS"; // Escapa la barra invertida en el nombre del servidor
 //10.120.3.239 ip del servidor
 $CONNECT = array(
     "Database" => "T-Store",
     "UID" => "sa", // Cambié "Usuario" a "UID"
-    "PWD" => "1234" // Cambié "contraseña" a "PWD"
+    "PWD" => "123" // Cambié "contraseña" a "PWD"
 );
 
 // Establecer la conexión
