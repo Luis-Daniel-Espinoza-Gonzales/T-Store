112ab19c869660874d76724d02b982a61aaabbba
Luis Daniel Espinoza Gonzales <danidex129@gmail.com>
2025-10-16 11:59:26 -0300

    subo diseño de opciones para stock sucursal

--- php/stock_sucursal.php
+++ php/stock_sucursal.php
@@ -27,8 +27,23 @@ session_start(); // Inicia la sesión
             <li class="li-001">addslashes</li>
         </ul>
     </nav>
-    <main class="main" id="main">
-        
+    <main>
+        <div class="options">
+            <button class="option">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+            </button>
+            <button class="option">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
+        <div class="main" id="main">
+
+        </div>
     </main>
     <footer>
 
